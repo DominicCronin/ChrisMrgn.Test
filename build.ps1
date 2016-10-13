be63f0a51c0afec328e630ebd0b74dda716f6527
Dominic Cronin <dominic.cronin@indivirtual.com>
2016-10-13 21:12:01 +0200

    Tidied up build.ps1
    Signed-off-by: Dominic Cronin <dominic.cronin@gmail.com>

--- build.ps1
+++ build.ps1
@@ -1,26 +1,10 @@
-function Exec  
-{
-    [CmdletBinding()]
-    param(
-        [Parameter(Position=0,Mandatory=1)][scriptblock]$cmd,
-        [Parameter(Position=1,Mandatory=0)][string]$errorMessage = ($msgs.error_bad_command -f $cmd)
-    )
-    & $cmd
-    if ($lastexitcode -ne 0) {
-        throw ("Exec: " + $errorMessage)
-    }
-}
-
-
-""
-""
 
 if(Test-Path .\artifacts) { Remove-Item .\artifacts -Force -Recurse }
 
 
 "Restoring Packages"
 "============================="
-exec { & dotnet restore }
+dotnet restore
 
 
 
@@ -28,7 +12,7 @@ exec { & dotnet restore }
 ""
 "Running Unit Tests"
 "============================="
-exec { & dotnet test ChrisMrgn.Test.Core.Tests -c Release }
+dotnet test ChrisMrgn.Test.Core.Tests -c Release
 
 ""
 ""
@@ -36,7 +20,7 @@ exec { & dotnet test ChrisMrgn.Test.Core.Tests -c Release }
 "Creating Nuget Packages"
 "============================="
 
-$revision = @{ $true = $env:APPVEYOR_BUILD_NUMBER; $false = 1 }[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
+$revision = if ($env:APPVEYOR_BUILD_NUMBER -ne $null){$env:APPVEYOR_BUILD_NUMBER} else {1}
 $revision = "{0:D4}" -f [convert]::ToInt32($revision, 10)
-exec { & dotnet pack ChrisMrgn.Test.Core -c Release -o .\artifacts --version-suffix=build-$revision }  
-exec { & dotnet pack ChrisMrgn.Test.Models -c Release -o .\artifacts --version-suffix=build-$revision }  
+dotnet pack ChrisMrgn.Test.Core -c Release -o .\artifacts --version-suffix=build-$revision
+dotnet pack ChrisMrgn.Test.Models -c Release -o .\artifacts --version-suffix=build-$revision
