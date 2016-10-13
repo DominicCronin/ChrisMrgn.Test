6851a9ca9fe8968b822bf77b91115af2420e120e
Chris Morgan <chris@mrgn.co>
2016-10-13 07:57:29 -0700

    updated

---
+++ ChrisMrgn.Test.Core.Tests/Class1.cs
@@ -0,0 +1,17 @@
+﻿using Xunit;
+
+namespace ChrisMrgn.Test.Core.Tests
+{
+    public class Class1
+    {
+        public Class1()
+        {
+        }
+
+        [Fact]
+        public void IsTrue()
+        {
+            Assert.True(true);
+        }
+    }
+}
--- build.ps1
+++ build.ps1
@@ -11,14 +11,32 @@ function Exec
     }
 }
 
+
+""
+""
+
 if(Test-Path .\artifacts) { Remove-Item .\artifacts -Force -Recurse }
 
+
+"Restoring Packages"
+"============================="
 exec { & dotnet restore }
+
+
+
+""
+""
+"Running Unit Tests"
+"============================="
+exec { & dotnet test ChrisMrgn.Test.Core.Tests -c Release }
+
+""
+""
+
+"Creating Nuget Packages"
+"============================="
 
 $revision = @{ $true = $env:APPVEYOR_BUILD_NUMBER; $false = 1 }[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
 $revision = "{0:D4}" -f [convert]::ToInt32($revision, 10)
-
-#exec { & dotnet test Chrisr -c Release }
-
 exec { & dotnet pack ChrisMrgn.Test.Core -c Release -o .\artifacts --version-suffix=build-$revision }  
 exec { & dotnet pack ChrisMrgn.Test.Models -c Release -o .\artifacts --version-suffix=build-$revision }  
